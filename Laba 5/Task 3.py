c9be7d0d8bb677e6fad961c87bed640241b83ea2
badayelme <olegrakovice355@gmail.com>
2025-06-17 18:28:13 +0700

    practic_og

--- Laba 5/Task 3.py
+++ Laba 5/Task 3.py
@@ -1,4 +1,10 @@
 import random
-array = [random.randint(1,100) for i in range(20)]
-array.sort()
-print(array)
+array = [random.randint(1,100) for i in range(100)]
+array_2 = []
+for i in array:
+    if i % 2 == 0:
+        array_2.append(i)
+    else:
+        array.remove(i)
+array_2.sort()
+print(array_2)
